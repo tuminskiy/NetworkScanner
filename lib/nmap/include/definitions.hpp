494da8ca02b2478462ec349b172fd4a9f672e388
tuminskiy <unknowuzvername@gmail.com>
2021-03-21 17:15:51 +0300

    Leave only host

--- lib/nmap/include/definitions.hpp
+++ lib/nmap/include/definitions.hpp
@@ -1,72 +1,15 @@
 #pragma once
 
 #include <string>
-#include <deque>
-#include <ctime>
-#include <unordered_map>
+#include <vector>
 
 namespace nmap
 {
 
-struct Status
-{
-  enum class State { None = 0, Up, Down, Filtred };
-
-  State state;
-  std::string reason;
-};
-
-struct Service
-{
-  std::string name;
-  std::string method;
-  uint16_t conf;
-};
-
-struct Port
-{
-  enum Protocol { None = 0, Tcp, Udp };
-
-  Protocol protocol;
-  uint16_t portid;
-  Status status;
-  Service service;
-};
-
-using Ports = std::deque<Port>;
-
-
 struct Host
 {
-  Status status;
-  std::string mac;
   std::string address;
-  std::string vendor;
-  Ports ports;
-};
-
-using Hosts = std::deque<Host>;
-
-
-struct NmapResult
-{
-  time_t start_time;
-  time_t end_time;
-  Hosts hosts;
-};
-
-
-static const std::unordered_map<Port::Protocol, std::string> protocol_str = {
-  { Port::Protocol::None, "None" },
-  { Port::Protocol::Tcp, "Tcp" },
-  { Port::Protocol::Udp, "Udp" }
-};
-
-static const std::unordered_map<Status::State, std::string> state_str = {
-  { Status::State::None, "None" },
-  { Status::State::Up, "Up" },
-  { Status::State::Down, "Down" },
-  { Status::State::Filtred, "Filtred" }
+  std::string hostname;
 };
 
 } // namespace nmap
