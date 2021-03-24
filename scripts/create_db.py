23a2f05526e015ca7fc5580a05ad81e42797c4e9
tuminskiy <unknowuzvername@gmail.com>
2021-03-24 19:54:10 +0300

    Add create read-only user

--- scripts/create_db.py
+++ scripts/create_db.py
@@ -9,6 +9,9 @@ import configparser
 def exec_sql(auth, command):
   os.system(auth + " -c \"" + command + "\"")
 
+def exec_sql_for_db(auth, db, command):
+  os.system(auth + " -d " + db + " -c \"" + command + "\"")
+
 def exec_file_for_db(auth, db, file):
   os.system(auth + " -d " + db + " -f " + file)
 
@@ -34,10 +37,19 @@ new_user = config["DbConfig"]["new_user"]
 new_pass = config["DbConfig"]["new_pass"]
 new_db = config["DbConfig"]["new_db"]
 
+guest_user = config["DbConfig"]["guest_user"]
+guest_pass = config["DbConfig"]["guest_pass"]
 
-cmd_create_user = "CREATE USER " + new_user + " with PASSWORD '" + new_pass + "';"
+
+cmd_create_user = "CREATE USER " + new_user + " WITH PASSWORD '" + new_pass + "';"
+cmd_create_guest = "CREATE USER " + guest_user + " WITH PASSWORD '" + guest_pass + "';"
 cmd_create_db = "CREATE DATABASE " + new_db + ";"
+
 cmd_give_grant = "GRANT ALL PRIVILEGES ON DATABASE " + new_db + " TO " + new_user + ";"
+
+cmd_give_grant_usage = "GRANT USAGE ON SCHEMA public TO " + guest_user + ";"
+cmd_give_grant_select = "GRANT SELECT ON ALL TABLES IN SCHEMA public TO " + guest_user + ";"
+cmd_give_grant_alter = "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO " + guest_user + ";"
 
 cmd_create_host = "sql/host.sql"
 cmd_create_asset = "sql/asset.sql"
@@ -46,11 +58,16 @@ cmd_create_asset = "sql/asset.sql"
 auth = make_auth(username, host, port)
 
 exec_sql(auth, cmd_create_user)
+exec_sql(auth, cmd_create_guest)
 exec_sql(auth, cmd_create_db)
 exec_sql(auth, cmd_give_grant)
 
 
 auth = make_auth(new_user, host, port)
+
+exec_sql_for_db(auth, new_db, cmd_give_grant_usage)
+exec_sql_for_db(auth, new_db, cmd_give_grant_select)
+exec_sql_for_db(auth, new_db, cmd_give_grant_alter)
 
 exec_file_for_db(auth, new_db, cmd_create_host)
 exec_file_for_db(auth, new_db, cmd_create_asset)
