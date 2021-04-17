569cc139db49c538a6da17bbcff99f9f69543492
tuminskiy <unknowuzvername@gmail.com>
2021-04-17 11:05:42 +0300

    Make save host async

--- nscan/src/nscanservice/nscanservice.cpp
+++ nscan/src/nscanservice/nscanservice.cpp
@@ -6,6 +6,7 @@
 #include <boost/property_tree/xml_parser.hpp>
 #include <QDebug>
 #include <QDateTime>
+#include <future>
 
 namespace nscan
 {
@@ -44,9 +45,18 @@ Status NscanService::start_scan(ServerContext* context, const StartScanRequest* 
   std::vector<nmap::Host> hosts;
   nscan::read_result(nmap_result.get_child("nmaprun"), "host", hosts);
 
+  std::vector<std::future<unsigned int>> future_ids;
+  future_ids.reserve(hosts.size());
+
   for (const auto& host : hosts) {
-    if (const auto host_id = db_->save_host(host); host_id != 0)
-      res->add_host_id(host_id);
+    // const auto future_id = std::async(std::launch::async, &storage::Database::save_host, db_.get(), host);
+    // future_ids.push_back(std::move(future_id));
+    future_ids.emplace_back(std::async(std::launch::async, &storage::Database::save_host, db_.get(), host));
+  }
+
+  for (auto& future_id : future_ids) {
+    if (const auto id = future_id.get(); id != 0)
+      res->add_host_id(id);
   }
 
   return Status::OK;
